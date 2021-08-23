ea2f1123b617ba609b4bca2e1d35865cefed0fe1
abhinavkriti <abhinavkritii@gmail.com>
2021-08-23 18:44:34 +0530

    intial commit

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+AuthenticatorAssertionResponse
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
